37f7c3c55b6bd946d2bfcdee20495e4c9234bf42
Pavel Senchanka <pavel.senchanka@gmail.com>
2016-09-11 18:01:54 +0300

    Implemented end-to-end testing

--- src/e2e/scala/EndToEndTest.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest._
-
-class EndToEndTest extends FunSpec with Matchers with OptionValues with Inside with Inspectors {
-
-  describe("Something") {
-    it ("should do something") {}
-  }
-}
